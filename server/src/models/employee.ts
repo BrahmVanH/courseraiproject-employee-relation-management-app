65d3a8fe1cc0ebc6557f62bd10db2349b59847d7
Brahm Van Houzen <brahmvanh@gmail.com>
2024-10-05 12:59:51 -0400

    added virtuals for full names in patient and employee models

--- server/src/models/employee.ts
+++ server/src/models/employee.ts
@@ -1,13 +1,22 @@
 import mongoose, { Schema } from 'mongoose';
 import { Employee } from '../types';
 
-const employeeSchema = new Schema<Employee>({
-	firstName: { type: String, required: true, maxlength: 50, minlength: 2 },
-  lastName: { type: String, required: true, maxlength: 50, minlength: 2 },
-	email: { type: String, required: true, unique: true },
-	department: { type: Schema.Types.ObjectId, ref: 'Department' },
-	patients: [{ type: Schema.Types.ObjectId, ref: 'Patient' }],
-  projects: [{ type: Schema.Types.ObjectId, ref: 'Project' }],
+const employeeSchema = new Schema<Employee>(
+	{
+		firstName: { type: String, required: true, maxlength: 50, minlength: 2 },
+		lastName: { type: String, required: true, maxlength: 50, minlength: 2 },
+		email: { type: String, required: true, unique: true },
+		department: { type: Schema.Types.ObjectId, ref: 'Department' },
+		patients: [{ type: Schema.Types.ObjectId, ref: 'Patient' }],
+		projects: [{ type: Schema.Types.ObjectId, ref: 'Project' }],
+	},
+	{
+		virtuals: true,
+	}
+);
+
+employeeSchema.virtual('fullName').get(function (this: Employee) {
+  return `${this.firstName} ${this.lastName}`;
 });
 
 const Employee = mongoose.model<Employee>('Employee', employeeSchema);
--- server/src/models/patient.ts
+++ server/src/models/patient.ts
@@ -1,11 +1,19 @@
 import mongoose from 'mongoose';
 import { Patient } from '../types';
 
+const patientSchema = new mongoose.Schema<Patient>(
+	{
+		firstName: { type: String, required: true, maxlength: 50, minlength: 2 },
+		lastName: { type: String, required: true, maxlength: 50, minlength: 2 },
+		age: { type: Number, required: true },
+	},
+	{
+		virtuals: true,
+	}
+);
 
-const patientSchema = new mongoose.Schema<Patient>({
-  firstName: { type: String, required: true, maxlength: 50, minlength: 2 },
-  lastName: { type: String, required: true, maxlength: 50, minlength: 2 },
-  age: { type: Number, required: true },
+patientSchema.virtual('fullName').get(function (this: Patient) {
+	return `${this.firstName} ${this.lastName}`;
 });
 
 const Patient = mongoose.model<Patient>('Patient', patientSchema);
